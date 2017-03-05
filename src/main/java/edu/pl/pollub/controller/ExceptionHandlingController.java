409c0908c06a28b21c85bec7ed8aa5bc8b49cd0b
Dell <jaroslaw.bielec@pollub.edu.pl>
2017-03-05 01:21:40 +0100

    Exceptions handling is created

--- src/main/java/edu/pl/pollub/controller/ExceptionHandlingController.java
+++ src/main/java/edu/pl/pollub/controller/ExceptionHandlingController.java
@@ -1,6 +1,7 @@
 package edu.pl.pollub.controller;
 
 import edu.pl.pollub.exception.PageNotExistException;
+import edu.pl.pollub.exception.StorageFileNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,12 @@ import java.io.IOException;
  */
 @Controller
 public class ExceptionHandlingController {
+    @ExceptionHandler({PageNotExistException.class,StorageFileNotFoundException.class})
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public void handlePageNotFoundException(IllegalStateException ex, HttpServletResponse response) throws IOException
+    {
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public void handleException(IllegalStateException ex, HttpServletResponse response) throws IOException
